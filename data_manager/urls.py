7da9ca9f4192f49d9b514d199c5a4bc8f1a90d70
PGensel <gensel@burando.de>
2021-11-12 17:16:25 +0100

    Added to CenterOfMass/urls.py - data_manager.urls - data_explorer.urls
    Added to data_manager/models.py
    - Case
    - Masspoint
    
    Added to data_manager/forms.py
    - NewCaseForm
    - EditCaseForm
    - AddMasspointForm
    - EditMasspointForm

---
+++ data_manager/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name = "data_manager"
+
+urlspatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('new/', views.NewCaseView, name="new_case"),
+    path('<int:case_id>/', views.OverviewCaseView.as_view(), name="overview_case"),
+    path('<int:case_id>/add/', views.AddMasspointView, name="add_masspoint"),
+    path('<int:case_id>/edit/', views.EditCaseView, name="edit_case"),
+    path('<int:case_id>/delete/', views.DeleteCaseView, name="delete_case"),
+    path('<int:case_id>/<int:masspoint_id>/edit/', views.EditMasspointView, name="edit_masspoint"),
+    path('<int:case_id>/<int:masspoint_id>/delete/', views.DeleteMasspointView, name="delete_masspoint"),
+]
